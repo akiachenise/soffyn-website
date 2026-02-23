4318815c1e337388407df498059d503f5d6c4788
akiachenise <akiachenise@gmail.com>
2026-02-23 09:42:07 -0600

    script updates

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
 document
   .getElementById("waitlist-form")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    const form = this;
-    const email = form.querySelector('input[type="email"]').value;
-    const fname = form.querySelector('input[type="text"]').value;
-
-    const url =
-      form.action.replace("/post?", "/post-json?") +
-      "&EMAIL=" +
-      encodeURIComponent(email) +
-      "&FNAME=" +
-      encodeURIComponent(fname) +
-      "&c=callback";
-
-    const script = document.createElement("script");
-    script.src = url;
-    document.body.appendChild(script);
-
-    window.callback = function (data) {
-      document.body.removeChild(script);
+  .addEventListener("submit", function () {
+    setTimeout(function () {
       window.location.href = "/thank-you.html";
-    };
+    }, 1000);
   });
