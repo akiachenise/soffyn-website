1584cc506e89cbceeef27b4bc9e5cac4b56fb57e
akiachenise <akiachenise@gmail.com>
2026-02-23 09:19:17 -0600

    script updates

--- script.js
+++ script.js
@@ -4,13 +4,23 @@ document
     e.preventDefault();
 
     const form = this;
-    const data = new FormData(form);
+    const email = form.querySelector('input[type="email"]').value;
+    const fname = form.querySelector('input[type="text"]').value;
 
-    fetch(form.action, {
-      method: "POST",
-      body: data,
-      mode: "no-cors",
-    }).then(function () {
+    const url =
+      form.action.replace("/post?", "/post-json?") +
+      "&EMAIL=" +
+      encodeURIComponent(email) +
+      "&FNAME=" +
+      encodeURIComponent(fname) +
+      "&c=callback";
+
+    const script = document.createElement("script");
+    script.src = url;
+    document.body.appendChild(script);
+
+    window.callback = function (data) {
+      document.body.removeChild(script);
       window.location.href = "/thank-you.html";
-    });
+    };
   });
