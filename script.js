0b30605695140b9eaa821201bba67ff4f20d167e
akiachenise <akiachenise@gmail.com>
2026-02-23 18:31:35 -0600

    script edits

--- script.js
+++
@@ -1,7 +0,0 @@
-document
-  .getElementById("waitlist-form")
-  .addEventListener("submit", function () {
-    setTimeout(function () {
-      window.location.href = "/thank-you.html";
-    }, 1500);
-  });
